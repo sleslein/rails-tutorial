3a458831af53ac6f6d08e11c3435997290a275af
Shea Leslein <sleslein@users.noreply.github.com>
2022-03-05 11:30:19 -0600

    update routing with resources and use 'article_path'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
   
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
