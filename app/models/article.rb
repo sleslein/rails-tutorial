bd6eb6e5c03f16fc6fdec98a79f0bf4746426ed8
Shea Leslein <sleslein@users.noreply.github.com>
2022-03-06 16:22:27 -0600

    added basic http authenticate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     include Visible
 
-    has_many :comments, dependant: :destroy
+    has_many :comments, dependent: :destroy
     
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
